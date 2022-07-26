2db49cffd39ffcedaf13559477d2f35e768e7a30
Ritabrata Paul <67918498+Ritabrata1169@users.noreply.github.com>
2022-07-27 00:01:31 +0530

    first Commit

---
+++ my-amazon/src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import './App.css';
+import Header from './Header';
+import Home from './Home';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Checkout from "./Checkout";
+import Login from "./Login";
+
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+      
+        <Routes>
+          <Route path ='/' element = {<>
+          <Header/>
+          <Home/>
+          
+          </>}
+          /> 
+          <Route path ='/Checkout' element = {<>
+          <Header/>
+          <Checkout/>
+          
+          </>}
+          /> 
+          <Route path ='/Login' element = {<Login/>}/>
+          
+          
+                   
+          
+        </Routes>
+      </div>
+    </Router>
+    
+  );
+}
+
+export default App;
---
+++ my-amazon/src/firebase.js
@@ -0,0 +1,13 @@
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDpQxreG4ptdwVwYsLLbqloLJQ3BSqsPeM",
+  authDomain: "clone-e24da.firebaseapp.com",
+  projectId: "clone-e24da",
+  storageBucket: "clone-e24da.appspot.com",
+  messagingSenderId: "736161088430",
+  appId: "1:736161088430:web:b22ed8953044f5b6107e3f",
+  measurementId: "G-JV6K9NG167"
+};
+
+
